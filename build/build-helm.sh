1d6037feff59087eb91bb38fd3425697e9c06e7e
Sune Koch Hansen <sune@kvalitetsit.dk>
2026-06-16 11:50:23 +0200

    fix build

--- build/build-helm.sh
+++ build/build-helm.sh
@@ -1,1 +1,2 @@
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/src alpine/helm:3 dependency update /src/chart
 docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/src alpine/helm:3 package /src/chart --app-version $2 --version $2 -d /src
